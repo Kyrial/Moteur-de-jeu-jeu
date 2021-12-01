ca62c4db98bf04dc3e5bbf017ac3a2f724140b18
Bardin Melvin <melvin.bardin@etu.umontpellier.fr>
2021-12-01 15:25:18 +0100

    correctif collision

--- Source/moc_mainwidget.cpp
+++ Source/moc_mainwidget.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWidget_t {
-    QByteArrayData data[6];
-    char stringdata0[54];
+    QByteArrayData data[7];
+    char stringdata0[64];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,15 +32,16 @@ struct qt_meta_stringdata_MainWidget_t {
 static const qt_meta_stringdata_MainWidget_t qt_meta_stringdata_MainWidget = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWidget"
-QT_MOC_LITERAL(1, 11, 14), // "cameraControle"
+QT_MOC_LITERAL(1, 11, 14), // "signalKeyPress"
 QT_MOC_LITERAL(2, 26, 0), // ""
-QT_MOC_LITERAL(3, 27, 13), // "keyPressEvent"
-QT_MOC_LITERAL(4, 41, 10), // "QKeyEvent*"
-QT_MOC_LITERAL(5, 52, 1) // "e"
+QT_MOC_LITERAL(3, 27, 10), // "QKeyEvent*"
+QT_MOC_LITERAL(4, 38, 1), // "e"
+QT_MOC_LITERAL(5, 40, 14), // "cameraControle"
+QT_MOC_LITERAL(6, 55, 8) // "keyPress"
 
     },
-    "MainWidget\0cameraControle\0\0keyPressEvent\0"
-    "QKeyEvent*\0e"
+    "MainWidget\0signalKeyPress\0\0QKeyEvent*\0"
+    "e\0cameraControle\0keyPress"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,20 +51,26 @@ static const uint qt_meta_data_MainWidget[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   29,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x0a /* Public */,
-       3,    1,   25,    2, 0x0a /* Public */,
+       5,    0,   32,    2, 0x0a /* Public */,
+       6,    1,   33,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
 
  // slots: parameters
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void, 0x80000000 | 3,    4,
 
        0        // eod
 };
@@ -74,9 +81,19 @@ void MainWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MainWidget *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->cameraControle(); break;
-        case 1: _t->keyPressEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 0: _t->signalKeyPress((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 1: _t->cameraControle(); break;
+        case 2: _t->keyPress((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MainWidget::*)(QKeyEvent * );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWidget::signalKeyPress)) {
+                *result = 0;
+                return;
+            }
         }
     }
 }
@@ -112,15 +129,22 @@ int MainWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
+}
+
+// SIGNAL 0
+void MainWidget::signalKeyPress(QKeyEvent * _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
